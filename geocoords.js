51a58d6991ecb8c873abd10fdbec06f630edd2fc
raghavsara <119783544+raghavsara@users.noreply.github.com>
2022-12-07 00:03:13 +0530

    Update geocoords.js

--- geocoords.js
+++ geocoords.js
@@ -1,8 +1,8 @@
 var map1 = L.map('map').setView([17.291038, 78.762219], 15);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map1);
 
 $.getJSON("foo.geojson",function(data){
